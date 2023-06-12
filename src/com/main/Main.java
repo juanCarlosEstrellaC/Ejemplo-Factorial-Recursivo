ede2132bdeed2d69ee4596ba9f6d604627165b59
Juan Estrella <jcestrella@uce.edu.ec>
2023-06-11 21:58:37 -0500

    Resumen de pasos para clonar

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,5 +1,12 @@
 package com.main;
 
+/**
+ * @author Lap
+ *	1. Clonar "git clone xxx"
+ *	2. Hacer "git checkout xxx" a la rama que deseo acceder
+ *	3. Crear una nueva rama partiendo desde la que deseo comenzar.
+ *	4. Hacer los commits que desee. 
+ */
 public class Main {
 
 	public static void main(String[] args) {
